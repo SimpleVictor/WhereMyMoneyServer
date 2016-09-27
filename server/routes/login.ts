d623103b8e9bf43229a2ecce22cb5a568b13fd74
VietAnhh <viet.le175@gmail.com>
2016-09-27 02:58:41 -0400

    more routes

--- server/routes/login.ts
+++ server/routes/login.ts
@@ -3,12 +3,13 @@ import { Router, Request, Response, NextFunction } from "express";
 const loginRouter: Router = Router();
 var requests = require('request');
 
+loginRouter.get("/", function(req,res,next){
+    res.json("bruhhh");
+});
+
 loginRouter.post("/", function (request: Request, response: Response, next: NextFunction) {
     let obj = request.body;
     console.log(obj);
-
-    
-
 });
 
 
